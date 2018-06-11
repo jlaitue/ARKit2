062cb873c272cf46e2bac5eed6b9cb3a31e9569b
Julian Lopez <jlechuga94@macbook-pro-de-julian.local>
2018-06-11 15:56:44 +0900

    Initial Commit

---
+++ World Tracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Julian Lechuga Lopez on 11/6/18.
+//  Copyright © 2018 Julian Lechuga Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
