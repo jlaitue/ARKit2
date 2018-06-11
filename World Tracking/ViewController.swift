a59bf9e0c53789b34381c806652bb78e1b74895f
Julian Lopez <jlechuga94@macbook-pro-de-julian.local>
2018-06-11 18:06:10 +0900

    Primer commit de inicio para pruebas con ARKIT2 en iPad

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius:0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0,0,0)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
 }
 
