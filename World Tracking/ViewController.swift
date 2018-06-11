19f6eb6af9a8280642a0e0f82393bd1881ea2cea
Julian Lopez <jlechuga94@macbook-pro-de-julian.local>
2018-06-11 19:26:06 +0900

    Cambios para agregar cubo, reset origin space y agregar otro cubo en nuevo punto de origen

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -20,12 +20,28 @@ class ViewController: UIViewController {
     }
 
 
+    
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
         node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius:0)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         node.position = SCNVector3(0,0,0)
         self.sceneView.scene.rootNode.addChildNode(node)
+        
     }
+    
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession(){
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes{(node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options:
+            [.resetTracking, .removeExistingAnchors] )
+    }
+    
 }
 
