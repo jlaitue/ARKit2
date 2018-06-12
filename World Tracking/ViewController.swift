9b85bb452bd5ba328374b2e1d35fa15ea9014d25
Julian Lopez <jlechuga94@macbook-pro-de-julian.local>
2018-06-12 16:37:13 +0900

    Actualización y pruebas de formas geométricas básicas

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,11 +24,50 @@ class ViewController: UIViewController {
     
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius:0)
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0,0,0)
-        self.sceneView.scene.rootNode.addChildNode(node)
         
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        
+        let cylinderNode = SCNNode(geometry: SCNCylinder(radius: 0.05, height: 0.05))
+        cylinderNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+//        Standard 3D geometry
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+//        node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.1, height: 0.5)
+//        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius:0.03)
+//        node.geometry = SCNSphere(radius: 0.1)
+//        node.geometry = SCNTube(innerRadius: 0.1, outerRadius: 0.2, height: 0.3)
+//        node.geometry = SCNTorus(ringRadius: 0.5, pipeRadius: 0.2)
+//        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+//        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        
+//        let path = UIBezierPath()
+//        path.move(to: CGPoint(x:0,y:0))
+//        path.addLine(to: CGPoint(x:0, y:0.2))
+//        path.addLine(to: CGPoint(x:0.2, y:0.3))
+//        path.addLine(to: CGPoint(x:0.4, y:0.2))
+//        path.addLine(to: CGPoint(x:0.4, y:0))
+//        let shape = SCNShape(path: path, extrusionDepth:0.05)
+//        node.geometry = shape
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+//        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+
+        node.position = SCNVector3(0.2,0.1,-0.2)
+        cylinderNode.position = SCNVector3(-0.3,0.2,-0.3)
+        boxNode.position = SCNVector3(0,-0.05,0)
+        doorNode.position = SCNVector3(0,-0.02,0.055)
+        node.addChildNode(cylinderNode)
+        node.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+        self.sceneView.scene.rootNode.addChildNode(node)
+//        self.sceneView.scene.rootNode.addChildNode(cylinderNode)
+
     }
     
     @IBAction func reset(_ sender: Any) {
@@ -41,6 +81,11 @@ class ViewController: UIViewController {
         }
         self.sceneView.session.run(configuration, options:
             [.resetTracking, .removeExistingAnchors] )
+    }
+    
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat{
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) +
+            min(firstNum,secondNum)
     }
     
 }
